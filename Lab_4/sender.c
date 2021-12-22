db6163efdf11037630a77ba6a1190e1dc3627031
Aman Verma <66964572+Aman-Verma-28@users.noreply.github.com>
2021-12-22 20:19:59 +0530

    changing sender.c

--- Lab_4/sender.c
+++ Lab_4/sender.c
@@ -9,62 +9,34 @@
 
 #define HELLO_PORT 12345
 #define HELLO_GROUP "225.0.0.37"
-#define MSGBUFSIZE 25
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in addr;
-    int fd, nbytes, addrlen;
+    int fd, cnt;
     struct ip_mreq mreq;
-    char msgbuf[MSGBUFSIZE];
+    char *message = "Aman";
 
-    u_int yes = 1; 
-    
     if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket");
         exit(1);
     }
 
-    
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0)
-    {
-        perror("Reusing ADDR failed");
-        exit(1);
-    }
-   
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_addr.s_addr = inet_addr(HELLO_GROUP);
     addr.sin_port = htons(HELLO_PORT);
 
-    
-    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        perror("bind");
-        exit(1);
-    }
-
-    
-    mreq.imr_multiaddr.s_addr = inet_addr(HELLO_GROUP);
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-    if (setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
-    {
-        perror("setsockopt");
-        exit(1);
-    }
-
-    
     while (1)
     {
-        addrlen = sizeof(addr);
-        if ((nbytes = recvfrom(fd, msgbuf, MSGBUFSIZE, 0,
-                               (struct sockaddr *)&addr, &addrlen)) < 0)
+        if (sendto(fd, message, sizeof(message), 0, (struct sockaddr *)&addr,
+                   sizeof(addr)) < 0)
         {
-            perror("recvfrom");
+            perror("sendto");
+            exit(1);
         }
-        puts(msgbuf);
+        sleep(1);
     }
-
     return 0;
 }
